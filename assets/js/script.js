2472c36a0cf5ac98c8d308de3dd8e1eeace836a4
Nsc86 <nicolas.salcedo86@gmail.com>
2022-03-05 23:55:21 -0300

    Version final 1.5 toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
+$(document).ready(function() {
+  //Efecto smooth al hacer click en lista Navbar
   $("a").on('click', function(event) {
 
     if (this.hash !== "") {
@@ -8,11 +8,34 @@ $(document).ready(function(){
       var hash = this.hash;
 
       $("html, body").animate({
-        scrollTop: $(hash).offset().top -40
-      }, 1200, function(){
+        scrollTop: $(hash).offset().top - 40
+      }, 1200, function() {
 
         window.location.hash = hash;
       });
     }
   });
+
+  //Oculta texto en tarjeta cuando se hace click en la imagen
+
+  $("#c1").click(function() {
+
+    $("#card1").toggle("slow");
+  });
+
+  $("#c2").click(function() {
+
+    $("#card2").toggle("slow");
+  });
+
+  $("#c3").click(function() {
+
+    $("#card3").toggle("slow");
+  });
+
+  $("#c4").click(function() {
+
+    $("#card4").toggle("slow");
+  });
+
 });
